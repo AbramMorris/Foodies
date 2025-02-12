5ad500642473bb9ff494a5720b51f71807bdb126
Abram morris <abram.morris62@gmail.com>
2025-02-12 09:16:01 +0200

    creating HomeViewInterface

--- app/src/main/java/com/example/foodrecpie/HomeViewInterface.java
+++ app/src/main/java/com/example/foodrecpie/HomeViewInterface.java
@@ -3,9 +3,10 @@ package com.example.foodrecpie;
     import com.example.foodrecpie.Model.MealsPOJO;
 
     import java.util.ArrayList;
+    import java.util.List;
 
-    public interface HomeViewInterface {
-        public void showRandomMeals(ArrayList<MealsPOJO> randomMeals);
+public interface HomeViewInterface {
+        public void showRandomMeals(MealsPOJO randomMeals);
     }
 
 
